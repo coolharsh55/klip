b647acdc78207b3ee35238e6218fb50c6542d957
Emre Yılmaz <mail@emreyilmaz.me>
2014-01-19 20:56:07 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='klip',
-    version='0.1',
+    version='0.2',
     packages=['klip'],
     url='http://github.com/emre/klip',
     license='MIT',
